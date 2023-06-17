4f66bba738f6ee186bbe280a6289ea92618f7744
Fredagsmys <max.mattsson00@gmail.com>
2023-06-17 12:16:48 +0200

    fixed get_water_status

--- app.py
+++ app.py
@@ -28,40 +28,41 @@ def set_water_status():
 
 
 
-@app.route("/send_data")
+@app.route("/send_data", methods = ['GET', 'POST'])
 def recv_data():
-    print(request.is_json)
-    status = 0
-    print(request.json)
-    req = request.get_json()
-    print(req)
-    jsonmsg = json.loads(req)
-    print(jsonmsg)
-    value = jsonmsg["value"]
-    sensorType = jsonmsg["sensorType"]
-    sensorID = jsonmsg["sensorID"]
-    try:
-        connection = mysql.connector.connect(host='localhost',
-                                            database='sensorData',
-                                            user='root',
-                                            password='Baggenq321')
-        if connection.is_connected():
-            db_Info = connection.get_server_info()
-            print("Connected to MySQL Server version ", db_Info)
-            cursor = connection.cursor()
-            cursor.execute("select database();")
-            record = cursor.fetchone()
-            print("You're connected to database: ", record)
-            cursor.execute(f"INSERT INTO data (VALUE, SENSORTYPE, SENSORID) VALUES ({value},{sensorType},{sensorID})")
-            connection.commit()
-            
-    except Error as e:
-        print("Error while connecting to MySQL", e)
-    finally:
-        if connection.is_connected():
-            cursor.close()
-            connection.close()
-            print("MySQL connection is closed")
+    if request.method == 'POST':
+        print(request.is_json)
+        status = 0
+        print(request.json)
+        req = request.get_json()
+        print(req)
+        jsonmsg = json.loads(req)
+        print(jsonmsg)
+        value = jsonmsg["value"]
+        sensorType = jsonmsg["sensorType"]
+        sensorID = jsonmsg["sensorID"]
+        try:
+            connection = mysql.connector.connect(host='localhost',
+                                                database='sensorData',
+                                                user='root',
+                                                password='Baggenq321')
+            if connection.is_connected():
+                db_Info = connection.get_server_info()
+                print("Connected to MySQL Server version ", db_Info)
+                cursor = connection.cursor()
+                cursor.execute("select database();")
+                record = cursor.fetchone()
+                print("You're connected to database: ", record)
+                cursor.execute(f"INSERT INTO data (VALUE, SENSORTYPE, SENSORID) VALUES ({value},{sensorType},{sensorID})")
+                connection.commit()
+                
+        except Error as e:
+            print("Error while connecting to MySQL", e)
+        finally:
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
+                print("MySQL connection is closed")
     
     # assume data is json {sensorID: 2, sensorType: 3, value: 13.4}
     
